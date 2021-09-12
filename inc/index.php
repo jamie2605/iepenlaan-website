2a50338aab9bf1c751d9f74cc3347158de5b9648
Classen Benjamin (CC-AD/EPC1) <benjamin.classen@de.bosch.com>
2021-09-12 15:02:15 +0200

    Add start page

--- inc/index.php
+++ inc/index.php
@@ -1,3 +1,8 @@
+---
+title: Startseite
+layout: default
+---
+
 <!-- Carousel
  ================================================== -->
 <div id="myCarousel" class="carousel slide" data-ride="carousel">
