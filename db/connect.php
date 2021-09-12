e7d558c164c92cc9c606445c80b3745426ece7f2
Classen Benjamin (CC-AD/EPC1) <benjamin.classen@de.bosch.com>
2021-09-12 15:59:34 +0200

    Removed no longer needed files

--- db/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dsn = 'mysql:dbname=iepenlaan;host=127.0.0.1';
-$user = 'iepenlaan';
-$password = 'zeeland18';
-$dbh = new PDO($dsn, $user, $password);
-
-?>
--- inc/heizung.php
+++
@@ -1,3 +0,0 @@
-<div class="container main-content">
-<iframe src="iepenlaan.ddns.net:1880/ui" height="100%" width="100%"></iframe>
-</div>
